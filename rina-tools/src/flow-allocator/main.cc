724a6238e14bf877cbb7be00e054152588ff8650
kristjoc <kristjoc@MacBookPro.local>
2025-11-24 21:27:44 +0100

    back to INFO

--- rina-tools/src/flow-allocator/main.cc
+++ rina-tools/src/flow-allocator/main.cc
@@ -124,7 +124,7 @@ int wrapped_main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    rina::initialize("DEBUG", "");
+    rina::initialize("INFO", "");
 
     if (listen) {
         /* Server mode */
